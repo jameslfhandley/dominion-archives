3235dcc290fb451746616a91bb31efcefe841378
James LF Handley <jlfhandley@gmail.com>
2019-07-08 09:09:47 +0200

    Update and rename connect.php to adresbok/connect.php

--- adresbok/connect.php
+++ adresbok/connect.php
@@ -1,17 +1,9 @@
 <?php
-/*
 $currenthost = "localhost";
 $currentuser = "root";
 $currentcode = "";
 $currentdb = "addressbook";
-*/
 
-
-
-$currenthost = "sql38.jnb2.host-h.net";
-$currentuser = "recruit";
-$currentcode = "terran12451A";
-$currentdb = "training0";
 
 
 $connect0 = mysqli_connect($currenthost,$currentuser,$currentcode,$currentdb);
